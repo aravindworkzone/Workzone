8934d48da94a1c9d40e75b56cc8bf595b6656bb5
ARAVIND <77529@TCS.Local>
2026-02-03 17:10:52 +0530

    footer fix

--- frontend/src/page/home.jsx
+++ frontend/src/page/home.jsx
@@ -1,5 +1,5 @@
 import Header from "../components/Header";
-import Footer from "../components/footer";
+import Footer from "../components/Footer";
 import LeftSidebar from "../section/LeftBar";
 import RightSidebar from "../section/RightBar";
 import MainLayout from "../section/MainLay";
