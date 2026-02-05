0830853a973ae2dcc43c1e696978fc28e2e50c0b
Aravind A <learnthingsbetterthenever@gmail.com>
2026-02-05 16:44:04 +0000

    Edit task.controller.js

--- backend/controller/task.controller.js
+++ backend/controller/task.controller.js
@@ -34,7 +34,7 @@ exports.AddTask = async (req, res) => {
         await Routine.create({
           ...baseData,
           link: link || null,
-          yearly: link == null ? true : false
+          yearly: link !== null ? true : false
         });
         break;
 
