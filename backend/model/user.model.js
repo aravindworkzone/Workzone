6e8715b96cf804a513142f13261073813ff7898e
Aravind A <learnthingsbetterthenever@gmail.com>
2026-02-09 12:01:38 +0000

    Edit user.model.js

--- backend/model/user.model.js
+++ backend/model/user.model.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   password: { type: String, required: true },
   emailToken:{ type: String },
   emailTokenExpired:{ type: String },
-  emailVerify:{ type: Boolean , default: false},
+  emailVerify:{ type: Boolean , default: true},
 }, { timestamps: true });
 
 const User = mongoose.model('UserAuth', userSchema);
